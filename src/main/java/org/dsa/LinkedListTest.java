ca659da9b7e19eca908bd3f05700f6122185acb1
Nagesha S <43410978+NageshaSriram@users.noreply.github.com>
2025-03-09 09:49:52 +0530

    Circular and Doubling linked list test

--- src/main/java/org/dsa/LinkedListTest.java
+++ src/main/java/org/dsa/LinkedListTest.java
@@ -1,6 +1,8 @@
 package org.dsa;
 
-public class LinkedListTest {
+import javax.print.attribute.standard.SheetCollate;
+
+class SinglyLinkedList {
     static class Node {
         int data;
         Node next;
@@ -79,23 +81,163 @@ public class LinkedListTest {
             }
         }
     }
+}
+
+class DoublyLinkedList {
+    static class Node {
+        int data;
+        Node pre, next;
+
+        Node(int data) {
+            this.data = data;
+            this.pre = null;
+            this.next = null;
+        }
+    }
+
+    private Node head;
+
+    public void insert(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+        newNode.pre = temp;
+    }
+
+    public void displayNodes() {
+        System.out.println("-------Displaying Nodes-------");
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data+ " <-> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public void delete(int data) {
+        System.out.println("---Deleting item---"+data);
+        if (isEmpty()) {
+            System.out.println("List is empty");
+            return;
+        }
+
+        if (head.data == data) {
+            head = head.next;
+            if (head != null) {
+                head.pre = null;
+            }
+            return;
+        }
+
+        Node temp = head;
+        while (temp != null && temp.data != data) {
+            temp = temp.next;
+        }
+        if (temp == null) return;
+        if (temp.pre != null) {
+            temp.pre.next = temp.next;
+        }
+        if (temp.next != null) {
+            temp.next.pre = temp.pre;
+        }
+        System.out.println("No matching item on: "+data);
+    }
+}
+
+class CircularLinkedList {
+    private class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    private Node last;
+
+    public void insert(int data) {
+        Node newNode = new Node(data);
+        if (last == null) {
+            last = newNode;
+            last.next = last;
+        } else {
+            newNode.next = last.next;
+            last.next = newNode;
+            last = newNode;
+        }
+    }
+
+    public void displayNodes() {
+        if (last == null) return;
+        Node temp = last.next;
+        do {
+            System.out.println(temp.data + " -> ");
+            temp = temp.next;
+        } while (temp != last.next);
+        System.out.println("(back to head)");
+    }
+}
+
+public class LinkedListTest {
 
     public static void main(String[] args) {
-        LinkedListTest linkedListTest = new LinkedListTest();
-        linkedListTest.insert(10);
-        linkedListTest.insert(20);
-        linkedListTest.insert(30);
-        linkedListTest.displayNodes();
-        System.out.println("Found at ::" + linkedListTest.search(20));
-        System.out.println("Found at ::" + linkedListTest.search(30));
-        System.out.println("Found at ::" + linkedListTest.search(10));
-        System.out.println("Found at ::" + linkedListTest.search(0));
-        linkedListTest.insertAtBegging(5);
-        linkedListTest.displayNodes();
-        linkedListTest.delete(30);
-        linkedListTest.displayNodes();
-        linkedListTest.delete(5);
-        linkedListTest.displayNodes();
+        System.out.println("--------------Singly Linked List--------------");
+        SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
+        singlyLinkedList.insert(10);
+        singlyLinkedList.insert(20);
+        singlyLinkedList.insert(30);
+        singlyLinkedList.displayNodes();
+        System.out.println("Found at ::" + singlyLinkedList.search(20));
+        System.out.println("Found at ::" + singlyLinkedList.search(30));
+        System.out.println("Found at ::" + singlyLinkedList.search(10));
+        System.out.println("Found at ::" + singlyLinkedList.search(0));
+        singlyLinkedList.insertAtBegging(5);
+        singlyLinkedList.displayNodes();
+        singlyLinkedList.delete(30);
+        singlyLinkedList.displayNodes();
+        singlyLinkedList.delete(5);
+        singlyLinkedList.displayNodes();
+
+        System.out.println("--------------Doubly Linked List--------------");
+        DoublyLinkedList doublyLinkedList = new DoublyLinkedList();
+        doublyLinkedList.isEmpty();
+        doublyLinkedList.insert(10);
+        doublyLinkedList.insert(20);
+        doublyLinkedList.insert(30);
+        doublyLinkedList.displayNodes();
+        doublyLinkedList.delete(20);
+        doublyLinkedList.displayNodes();
+        doublyLinkedList.delete(10);
+        doublyLinkedList.displayNodes();
+        doublyLinkedList.delete(30);
+        doublyLinkedList.displayNodes();
+        doublyLinkedList.delete(30);
+        doublyLinkedList.displayNodes();
+
+        System.out.println("--------Circular Linked List--------");
+        CircularLinkedList circularLinkedList = new CircularLinkedList();
+        circularLinkedList.insert(10);
+        circularLinkedList.insert(20);
+        circularLinkedList.insert(30);
+        circularLinkedList.insert(40);
+        circularLinkedList.insert(50);
+        circularLinkedList.insert(60);
+        circularLinkedList.displayNodes();
+
+
 
     }
 }
